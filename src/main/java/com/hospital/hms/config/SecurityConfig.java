31c76a11632cfd81017ab15daf8db91c670d21d0
Vinay Kumar Verma <106vermavinaykr@gmail.com>
2026-05-09 15:59:27 +0530

    feat: implement in-memory authentication with BCrypt password encoding

--- src/main/java/com/hospital/hms/config/SecurityConfig.java
+++ src/main/java/com/hospital/hms/config/SecurityConfig.java
@@ -4,10 +4,39 @@ package com.hospital.hms.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    //Password Encoder Bean
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //In-Memory User Authentication
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(
+            PasswordEncoder passwordEncoder){
+
+        UserDetails admin = User.builder()
+                .username("admin")
+
+                // Encrypt password Using BCrypt
+                .password(
+                        passwordEncoder.encode("admin123"))
+
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
 
     //Security Configuration for Rest APIs
     @Bean
@@ -19,9 +48,12 @@ public class SecurityConfig {
                 //Configure authorization rules
                 .authorizeHttpRequests(auth -> auth
 
-                        //Allow all requests temporarily
-                                .anyRequest().permitAll()
-                );
+                        //All requests require authentication
+                                .anyRequest().authenticated()
+                )
+                //Enable Basic Authentication
+                .httpBasic(httpBasic -> {});
+
         return http.build();
     }
 }
